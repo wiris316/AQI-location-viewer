770ea6465c66a03132ce61a66402572217269f16
Iris <wiris316@gmail.com>
2024-05-26 14:54:26 -0400

    add logic to fetch data for different cities

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import DetailsCard from "./DetailsCard";
 import Navbar from "./Navbar";
 import "../assets/Dashboard.scss";
 
+interface ILocationDetails {
+  city: string, 
+  aqi: number, 
+  lastUpdated: Date,
+  category: string
+}
+
 const Dashboard = () => {
+  const [locationDetails, setLocationDetails] = useState<ILocationDetails>({ city: '', aqi: 0, lastUpdated: new Date(), category: '' });
+  
   return (
     <div id="Dashboard">
       <h1>AQI Location Viewer</h1>
-      <Navbar />
+      <Navbar locationDetails={locationDetails} setLocationDetails={setLocationDetails} />
       <DetailsCard />
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,90 @@
-import React from "react";
+import React, { useCallback, useEffect, useState, SetStateAction } from "react";
 import "../assets/Navbar.scss";
-import { fetchLocationData } from "../server/api";
 
-const Navbar = () => {
-  const showLocation = async (location: string) => {
+interface LocationDetails {
+  city: string;
+  aqi: number;
+  lastUpdated: Date;
+  category: string;
+}
+
+interface NavbarProps {
+  locationDetails: LocationDetails;
+  setLocationDetails: React.Dispatch<SetStateAction<LocationDetails>>;
+}
+
+const Navbar = ({ locationDetails, setLocationDetails }: NavbarProps) => {
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const showLocationData = useCallback(async () => {
     try {
-      const res = await fetchLocationData(location);
-      console.log('res',res)
+      const res = await fetchLocationData(selectedLocation);
+      setLocationDetails({
+        ...locationDetails,
+        city: res.city.name,
+        aqi: res.aqi,
+        lastUpdated: new Date(),
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  };
+  }, [selectedLocation]);
+  
+  useEffect(() => {
+    showLocationData();
+  },[selectedLocation])
+
+  async function fetchLocationData(city: string) {
+    try {
+      const res = await fetch(`/api/location/${city}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.status === 200) {
+        const data = await res.json();
+        if (data.status !== "error") {
+          return data.data;
+        } else {
+          throw new Error(`Error: ${data.data}`);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <nav>
       <ul id="navbar-list">
-        <li className="navbar-item" onClick={() => showLocation("here")}>
+        <li
+          className="navbar-item"
+          id="here"
+          onClick={(e) => setSelectedLocation(e.currentTarget.id)}
+        >
           My Location
         </li>
-        <li className="navbar-item">Location 1</li>
-        <li className="navbar-item">Location 2</li>
-        <li className="navbar-item">Location 3</li>
+        <li
+          className="navbar-item"
+          id="Austin"
+          onClick={(e) => setSelectedLocation(e.currentTarget.id)}
+        >
+          Austin
+        </li>
+        <li
+          className="navbar-item"
+          id="Chicago"
+          onClick={(e) => setSelectedLocation(e.currentTarget.id)}
+        >
+          Chicago
+        </li>
+        <li
+          className="navbar-item"
+          id="Seattle"
+          onClick={(e) => setSelectedLocation(e.currentTarget.id)}
+        >
+          Seattle
+        </li>
       </ul>
     </nav>
   );
